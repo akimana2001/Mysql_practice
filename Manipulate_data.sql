f8dc8ab009f5d3844b9bff862d76bf8d29017929
akimana2001 <dativeakimana@gmail.com>
2026-03-04 18:39:46 +0200

    Implement data manipulation operations including update, delete, and select queries

---
+++ Manipulate_data.sql
@@ -0,0 +1,30 @@
+
+
+UPDATE Books
+SET price = price + 5
+WHERE title = 'Things Fall Apart';
+
+
+UPDATE Books
+SET stock_quantity = 25
+WHERE title = 'Atomic Habits';
+
+
+DELETE FROM Customers
+WHERE email = 'dativeakimana@gmail.com';
+
+
+DELETE FROM Orders
+WHERE order_id = 3;
+
+
+
+SELECT * FROM Books
+WHERE genre = 'Fiction';
+
+SELECT * FROM Orders
+WHERE total_amount > 50;
+
+
+SELECT * FROM Customers
+WHERE last_name LIKE '%son%';
